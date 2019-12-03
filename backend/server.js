c40a1b981a4fbd9b2b119946442d52e0135f96bf
Adam <adam.ferguson124@gmail.com>
2019-12-03 12:01:09 -0500

    route to middleware

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const MongoClient = require('mongodb').MongoClient;
 app.use(cors());
 app.use(express.json());
 
+// <------ Database connection ------>
 const uri = process.env.ATLAS_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // Go ahead and use the MongoDB full driver settings and sub the uri string with the 
@@ -21,6 +22,14 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+// <------ API Routes ------>
+// const exercisesRouter = require('./routes/exercises');
+// const usersRouter = require('./routes/users');
+
+// app.use('/exercises', exercisesRouter);
+// app.use('/users', usersRouter);
+
 
 app.listen(port, () => {
     console.log(`You're all set space cowboy. The app is running on port: ${port}`);
