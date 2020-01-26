b25d0fc50844be3951099e0fadcc3a3608a2b0f8
Adam <adam.ferguson124@gmail.com>
2020-01-25 21:39:31 -0500

    added redirect after user creation

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -27,6 +27,7 @@ export default class CreateUser extends Component {
         this.setState({
             username: ''
         })
+        setTimeout(()=>this.props.history.push('/create'), 500)
 
     }
     render() {
