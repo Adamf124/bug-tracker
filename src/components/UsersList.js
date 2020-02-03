996ca95e9bf0d24742a0cd181b1dc472086d6d36
Adam <adam.ferguson124@gmail.com>
2020-02-03 09:47:07 -0500

    displays users and date created

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+const User = props => (
+    <tr>
+        <td>{props.users.username}</td>
+        <td>{props.users.createdAt.substring(0,10)}</td>
+    </tr>
+)
 
 export default class UsersList extends Component {
+    constructor(props){
+        super(props);
+    this.state = {users: []}
+        }
+        componentDidMount() {
+            axios.get('/users/')
+            .then( response => {
+                console.log(response.data)
+                this.setState({ users: response.data})
+                console.log(this.state)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+        usersList() {
+            return this.state.users.map(users => {
+                console.log(users)
+              return <User 
+              users={users} 
+              key={users._id}
+              />;
+            })
+          }
     render() {
         return (
             <div>
-                
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Created On</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.usersList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
